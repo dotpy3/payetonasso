34f65865b6a6742010e40ab0d43cbf0f282d71ab
dotpy3 <moppapo@gmail.com>
2016-01-12 23:32:23 +0100

    added transaction fields to administration
    enhanced admin
    
    deleted binaries

--- payetonasso/admin.py
+++ payetonasso/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from payetonasso import models
 
-# Register your models here.
+
+@admin.register(models.Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+	date_hierarchy = 'created'
+	list_display = ('pk', 'name', 'message', 'price', 'fundation_name', 'fundation',
+			    	'created', 'creator', 'nemopay_article_id', 'notify_creator')
+
+
+@admin.register(models.IndividualTransaction)
+class IndividualTransactionAdmin(admin.ModelAdmin):
+	list_display = ('pk', 'transaction', 'user_name', 'user_email', 'state', 'validation')
+
+
+@admin.register(models.NemopayTransaction)
+class NemopayTransactionAdmin(admin.ModelAdmin):
+	date_hierarchy = 'created'
+	list_display = ('pk', 'inv_transaction', 'created', 'nemopay_id')
